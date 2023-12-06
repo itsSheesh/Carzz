cbf521d962dfcb6b5b5940c6e5ae84d1a664ced3
Mohamad amin <145129471+itsSheesh@users.noreply.github.com>
2023-12-06 15:09:37 +0330

    Let's Start CODING...

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,9 @@
-import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+     <h2 className="text-center text-3xl font-bold text-violet-500">Cars</h2>
     </div>
   );
 }
