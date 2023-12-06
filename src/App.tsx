c3539cabb3280a2ca1b321f37100a6f45a57503d
Mohamad amin <145129471+itsSheesh@users.noreply.github.com>
2023-12-06 18:16:37 +0330

    Nnavbar & Fonts

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,7 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-     <h2 className="text-center text-3xl font-bold text-violet-500">Cars</h2>
+      
     </div>
   );
 }
